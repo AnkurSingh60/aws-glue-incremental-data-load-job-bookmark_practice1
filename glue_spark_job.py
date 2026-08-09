0c278bd59c1a0bb52800a6ead1d914ab57fc5040
Ankur Singh <ankursinghh735@gmail.com>
2026-08-09 15:18:54 +0530

    Update glue_spark_job.py

--- glue_spark_job.py
+++ glue_spark_job.py
@@ -1,1 +1,32 @@
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from pyspark.sql import functions as F
+
+# Initialize Glue Context
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+# Read data from Glue Catalog
+df = spark.read.csv(
+    "s3://telecom-data-glue-1237/customer_subscription.csv",
+    header=True,
+    inferSchema=True
+)
+
+# Simple Transformation
+df_clean = df.dropDuplicates()
+
+# Write transformed data
+df_clean.write.mode("overwrite").parquet(
+    "s3://telecom-data-glue-1237/output/"
+)
+
+print("ETL Job Completed Successfully")
+
+
+
+
+
 
